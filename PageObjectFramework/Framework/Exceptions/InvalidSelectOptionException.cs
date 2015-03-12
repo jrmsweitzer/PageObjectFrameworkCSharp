ea33a9d821947176c1a0418d5b8093a1a1b98f55
jsweitzer@catalystitservices.com <jsweitzer@catalystitservices.com>
2015-03-12 11:32:22 -0400

    Added Custom Exceptions

--- PageObjectFramework/Framework/Exceptions/InvalidSelectOptionException.cs
+++ PageObjectFramework/Framework/Exceptions/InvalidSelectOptionException.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PageObjectFramework.Framework.Exceptions
 {
+    [Serializable]
     public class InvalidSelectOptionException : Exception
     {
         public InvalidSelectOptionException()
--- PageObjectFramework/Framework/Exceptions/WrongPageException.cs
+++ PageObjectFramework/Framework/Exceptions/WrongPageException.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace PageObjectFramework.Framework.Exceptions
 {
+    [Serializable]
     public class WrongPageException : Exception
     {
         public WrongPageException()
